2c52c00710692485c09d29d478f2829011b91a1f
FIRST_NAME LAST_NAME <mulianaibrahim@gmail.com>
2023-03-02 10:49:42 +0700

    inisiasi project

---
+++ operator-aritmatika.js
@@ -0,0 +1,50 @@
+// 5 Variabel yang di pakai untuk mengitung 3 angka
+console.log('operator aritmatika')
+let angka1 = 5 + 5 + 6
+let angka2 = 4 + 5 - 3
+let angka3 = 7 / 4 * 3
+let angka4 = 4 ** 5 + 7
+let angka5 = 4 % 5 - 3
+
+// Outpunya 
+penjumlahan = angka1 + angka2
+console.log('angka1 + angka2', penjumlahan)
+penjumlahan = angka3 + angka4
+console.log('angka3 + angka4', penjumlahan)
+penjumlahan = angka1 + angka5
+console.log('angka1 + angka5', penjumlahan)
+
+Pengurangan = angka1 - angka2
+console.log('angka1 - angka2', Pengurangan)
+Pengurangan = angka3 - angka4
+console.log('angka3 - angka4', Pengurangan)
+Pengurangan = angka1 - angka5
+console.log('angka1 - angka5', Pengurangan)
+
+penjumlahan = angka1 * angka2
+console.log('angka1 * angka2', penjumlahan)
+penjumlahan = angka3 * angka4
+console.log('angka3 * angka4', penjumlahan)
+penjumlahan = angka1 * angka5
+console.log('angka1 * angka5', penjumlahan)
+
+Perpangkatan = angka1 ** angka2
+console.log('angka1 ** angka2', Perpangkatan)
+Perpangkatan = angka3 ** angka4
+console.log('angka3 ** angka4', Perpangkatan)
+Perpangkatan = angka1 ** angka5
+console.log('angka1 ** angka5', Perpangkatan)
+
+Pembagian = angka1 / angka2
+console.log('angka1 / angka2', Pembagian)
+Pembagian = angka3 / angka4
+console.log('angka3 / angka4', Pembagian)
+Pembagian = angka1 / angka5
+console.log('angka1 / angka5', Pembagian)
+
+hasilBagi = angka1 % angka2
+console.log('angka1 % angka2', hasilBagi)
+hasilBagi = angka3 % angka4
+console.log('angka3 % angka4', hasilBagi)
+hasilBagi = angka1 % angka5
+console.log('angka1 % angka5', hasilBagi)
---
+++ operator-perbandingan.js
@@ -0,0 +1,25 @@
+console.log('operator perbandingan')
+let angka1 = 15 > 2;
+let angka2 = 20 > '4'
+let angka3 = 15 < 5
+let angka4 = 5 != 4
+
+perbandingan1 = angka1 > angka2
+console.log('angka1 > angka2', perbandingan1)
+perbandingan2 = angka3 < angka4
+console.log('angka3 < angka4', perbandingan2)
+
+perbandingan1 = angka1 = angka2
+console.log('angka1 = angka2', perbandingan1)
+perbandingan2 = angka3 = angka4
+console.log('angka3 = angka4', perbandingan2)
+
+perbandingan1 = angka1 >= angka2
+console.log('angka1 >= angka2', perbandingan1)
+perbandingan1 = angka1 >= angka2
+console.log('angka3 >= angka4', perbandingan2)
+
+perbandingan1 = angka1 != angka2
+console.log('angka1 != angka2', perbandingan1)
+perbandingan2 = angka1 != angka2
+console.log('angka3 != angka4', perbandingan2)
